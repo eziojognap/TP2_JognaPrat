a3443d5d0c79dece48224f03010925e42c528d94
eziojognap <eziojognap@gmail.com>
2023-04-17 11:23:20 -0300

    Tp finalizado sin documentacion

--- src/alumno.c
+++ src/alumno.c
@@ -7,6 +7,7 @@
 
 /************************************* INCLUDES ***************************************************/
 #include "alumno.h"
+#include <stdio.h>
 /************************************* EXTERN VARIABLES *******************************************/
 
 /************************************* PRIVATE MACROS AND DEFINES *********************************/
@@ -22,3 +23,44 @@
 /************************************* STATIC FUNCTIONS *******************************************/
 
 /************************************* GLOBAL FUNCTIONS *******************************************/
+static int SerializarCadena();
+static int SerializarNumero();
+
+static int SerializarCadena(const char * campo, const char * valor, char * cadena, int espacio){
+    return snprintf(cadena, espacio, "\"%s\":\"%s\",", campo, valor);
+}
+
+static int SerializarNumero(const char * campo, int valor, char * cadena, int espacio){
+    return snprintf(cadena, espacio, "\"%s\":\"%d\",", campo, valor);
+}
+
+int Serializar(alumno_t alumno, char cadena [], uint32_t espacio){
+    int disponibles = espacio;
+    int resultado;
+
+    cadena[0] = '{';
+    cadena++;
+    disponibles--;
+    resultado = SerializarCadena("apellido", alumno->apellido, cadena, disponibles);
+
+    if(resultado > 0){
+        disponibles -= resultado;
+        cadena += resultado;
+        resultado = SerializarCadena("nombre", alumno->nombre, cadena, disponibles);
+    }
+
+    if(resultado > 0){
+        disponibles -= resultado;
+        cadena += resultado;
+        resultado = SerializarNumero("documento", alumno->documento, cadena, disponibles);
+    }
+
+    if(resultado > 0){
+        cadena += resultado;
+        *(cadena - 1) = '}';
+        resultado = espacio - disponibles;
+    }
+
+    return resultado;
+}
+
